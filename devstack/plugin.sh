b5c154f94ba7139fedcd08f91fe04bc42d5186e5
Davanum Srinivas <davanum@gmail.com>
2017-04-03 06:00:35 -0400

    Run some e2e tests against the kubernetes instance
    Change-Id: I6fdf5ce4fbf0e2c9073e162d862de5c350b949a2

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -69,11 +69,15 @@ function install_k8s_cloud_provider {
         git clone https://${CONFORMANCE_REPO} ${K8S_SRC}
     fi
     go get -u github.com/jteeuwen/go-bindata/go-bindata || true
+    go get -u github.com/cloudflare/cfssl/cmd/... || true
 
     # Run the script that builds kubernetes from source and starts the processes
     pushd ${K8S_SRC} >/dev/null
     hack/install-etcd.sh
-    run_process kubernetes "sudo -E hack/local-up-cluster.sh"
+    export PATH=${K8S_SRC}/third_party/etcd:$GOPATH/bin:${PATH}
+    export LOG_DIR=${SCREEN_LOGDIR:-/opt/stack/logs}
+    sudo mkdir -p $LOG_DIR
+    run_process kubernetes "sudo -E PATH=$PATH hack/local-up-cluster.sh"
     popd >/dev/null
 }
 
