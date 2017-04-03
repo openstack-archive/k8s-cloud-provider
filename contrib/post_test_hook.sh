54455b0a7c5b35b8bad05f7029c2deabca6b8ff5
Davanum Srinivas <davanum@gmail.com>
2017-04-03 06:00:35 -0400

    [WIP] Testing hooks
    Change-Id: I6fdf5ce4fbf0e2c9073e162d862de5c350b949a2

--- contrib/post_test_hook.sh
+++ contrib/post_test_hook.sh
@@ -14,5 +14,65 @@
 
 # This script is executed inside post_test_hook function in devstack gate.
 
+export GOPATH=${BASE_DIR}/go
+
 cd $BASE/new/devstack
 source openrc admin admin
+
+echo "In post_test_hook"
+
+# Get the latest stable version of kubernetes
+export K8S_VERSION=$(curl -sS https://storage.googleapis.com/kubernetes-release/release/stable.txt)
+echo "K8S_VERSION : ${K8S_VERSION}"
+
+echo "Download Kubernetes CLI"
+sudo wget -O kubectl "http://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/amd64/kubectl"
+sudo chmod 755 kubectl
+./kubectl get nodes
+
+echo "Waiting for kubernetes service to start..."
+for i in {1..600}
+do
+    running_count=$(./kubectl -s=http://127.0.0.1:8080 get svc --no-headers 2>/dev/null | grep "443" | wc -l)
+    if [ "$running_count" -ge 1 ]; then
+      break
+    fi
+    echo -n "."
+    sleep 1
+done
+
+echo "Cluster created!"
+echo ""
+
+echo "Dump Kubernetes Objects..."
+./kubectl -s=http://127.0.0.1:8080 get componentstatuses
+./kubectl -s=http://127.0.0.1:8080 get configmaps
+./kubectl -s=http://127.0.0.1:8080 get daemonsets
+./kubectl -s=http://127.0.0.1:8080 get deployments
+./kubectl -s=http://127.0.0.1:8080 get events
+./kubectl -s=http://127.0.0.1:8080 get endpoints
+./kubectl -s=http://127.0.0.1:8080 get horizontalpodautoscalers
+./kubectl -s=http://127.0.0.1:8080 get ingress
+./kubectl -s=http://127.0.0.1:8080 get jobs
+./kubectl -s=http://127.0.0.1:8080 get limitranges
+./kubectl -s=http://127.0.0.1:8080 get nodes
+./kubectl -s=http://127.0.0.1:8080 get namespaces
+./kubectl -s=http://127.0.0.1:8080 get pods
+./kubectl -s=http://127.0.0.1:8080 get persistentvolumes
+./kubectl -s=http://127.0.0.1:8080 get persistentvolumeclaims
+./kubectl -s=http://127.0.0.1:8080 get quota
+./kubectl -s=http://127.0.0.1:8080 get resourcequotas
+./kubectl -s=http://127.0.0.1:8080 get replicasets
+./kubectl -s=http://127.0.0.1:8080 get replicationcontrollers
+./kubectl -s=http://127.0.0.1:8080 get secrets
+./kubectl -s=http://127.0.0.1:8080 get serviceaccounts
+./kubectl -s=http://127.0.0.1:8080 get services
+
+
+echo "Running tests..."
+pushd ${GOPATH}/src/k8s.io/kubernetes >/dev/null
+make all WHAT=cmd/kubectl
+make all WHAT=vendor/github.com/onsi/ginkgo/ginkgo
+make all WHAT=test/e2e/e2e.test
+sudo -E PATH=$PATH go run hack/e2e.go -- -v --test --test_args="--ginkgo.focus=\[Conformance\]"
+popd >/dev/null
