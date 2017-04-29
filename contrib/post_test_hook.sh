f697a4960fcd68c25a5818ccb403ce8ad9265bcc
Davanum Srinivas <davanum@gmail.com>
2017-04-29 17:32:55 -0400

    Set CLOUD_PROVIDER as local
    Change-Id: I41be1a84f2af8e9587ef635d7f1798c4d9ea196f

--- contrib/post_test_hook.sh
+++ contrib/post_test_hook.sh
@@ -157,5 +157,5 @@ sudo iptables -t nat -A POSTROUTING -o ens3 -s 10.0.0.0/24 -j MASQUERADE
 sudo iptables -t nat -A POSTROUTING -o ens3 -s 172.17.0.0/24 -j MASQUERADE
 
 sudo -E PATH=$GOPATH/bin:$PATH make all WHAT=test/e2e/e2e.test
-sudo -E PATH=$GOPATH/bin:$PATH go run hack/e2e.go -- -v --test --test_args="--ginkgo.trace=true --ginkgo.seed=1378936983 --logtostderr --v 4 --report-dir=/opt/stack/logs/ --ginkgo.v --ginkgo.skip=$(test_names)"
+sudo -E PATH=$GOPATH/bin:$PATH go run hack/e2e.go -- -v --test --test_args="--ginkgo.trace=true --ginkgo.seed=1378936983 --logtostderr --v 4 --provider=local --report-dir=/opt/stack/logs/ --ginkgo.v --ginkgo.skip=$(test_names)"
 popd >/dev/null
